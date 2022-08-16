172ace45da57e68608b8f34ae98f14cfcf6d8b68
Alexduuu <111344246+Alexduuu@users.noreply.github.com>
2022-08-16 16:07:04 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+  Queue<String> restaurantreserve = new LinkedList<>();
+    restaurantreserve.add("Ronn");
+    restaurantreserve.add("John");
+    restaurantreserve.add("Alex");
+    restaurantreserve.add("Mark");
+    restaurantreserve.add("Joven");
+
+    System.out.println("The queue is: " + restaurantreserve);
+
+    }
+}
+
+
+
+
+
